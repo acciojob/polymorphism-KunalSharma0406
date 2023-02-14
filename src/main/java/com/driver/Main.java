b1b85a4c1a9d4d51e113e0e1e3a544dd3f1e6e93
KunalSharma0406 <118346621+KunalSharma0406@users.noreply.github.com>
2023-02-14 11:58:21 +0530

    Assingment 2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
-
+    public static class Product{
+        public int product(int x, int y) {
+            int z= x*y;
+            return z;
+        }
+        public int product(int x, int y, int z) {
+            int a= x*y*z;
+            return a;
+        }
+        public double product(double x, double y) {
+            double z=x*y;
+            return z;
+        }
+    }
+public static void main(String[] args){
+        Product p= new Product();
+        p.product(5,10);
+        p.product(5 ,10);
+        p.product(5,10);
+}
 }
